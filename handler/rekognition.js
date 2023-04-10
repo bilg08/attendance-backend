6b4e47691234802ced4de7b60eb5c7cfcdad5225
BilguunLeap3 <bilguunleap3BilguunLeap3@iMAC2021-05s-iMac.local>
2023-04-10 15:23:54 +0800

    compare faces done

--- handler/rekognition.js
+++ handler/rekognition.js
@@ -1,6 +1,7 @@
 const Aws = require('aws-sdk');
 const rekognition = new Aws.Rekognition();
 const s3 = new Aws.S3();
+const axios = require('axios');
 module.exports.getPresignedUrlToSourceImages = async (event) => {
     const { email, ContentType } = JSON.parse(event.body);
     const ext = ContentType.split('/')[1];
@@ -13,10 +14,13 @@ module.exports.getPresignedUrlToSourceImages = async (event) => {
     const preSignedUrl = s3.getSignedUrl('putObject', params);
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify(
-          {
-            url: preSignedUrl,
-          },
+            {
+                url: preSignedUrl,
+            },
         ),
     };
 }
@@ -32,37 +36,77 @@ module.exports.getPresignedUrlToTargetImages = async (event) => {
     const preSignedUrl = s3.getSignedUrl('putObject', params);
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify(
-          {
-            url: preSignedUrl,
-          },
+            {
+                url: preSignedUrl,
+            },
         ),
     };
 }
 
 module.exports.compareFaces = async (event) => {
+    const { email } = JSON.parse(event.body);
     const response = await rekognition.compareFaces({
         SourceImage: {
             S3Object: {
                 Bucket: 'zorigooleap3devattendance',
-                Name: 'sourceimages/jeff.jpeg',
+                Name: `sourceimages/${email}.jpeg`,
             }
         },
         TargetImage: {
             S3Object: {
                 Bucket: 'zorigooleap3devattendance',
-                Name: 'targetimages/jeff.jpeg',
+                Name: `targetimages/${email}.jpeg`,
             }
         }
     }).promise();
     const { Similarity: similarity } = response?.FaceMatches[0];
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify({
-            data: { success: true, similarity }
-        })
-    };
+    if (similarity > 80) {
+        try {
+            await axios.post('https://uoa756xi8c.execute-api.us-east-1.amazonaws.com/dev/registerAttendance', {
+                email: email
+            });
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify({
+                    data: {
+                        success: true,
+                        message: 'amjilttai burtgelee'
+                    }
+                })
+            };
+        } catch (error) {
+            return {
+                statusCode: 400,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify({
+                    data: {
+                        success: false,
+                        message: 'Ta burtguulsen bain'
+                    }
+                })
+            };
+        }
+    }else{
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                data: {
+                    success: false,
+                    message: 'Ta bish baina'
+                }
+            })
+        };
+    }
 }
